82de5ac7c25f58a7fffa0672de6db603fe321f04
Paul Rowlands <paulrowlands@digitalitysolutions.co.uk>
2026-03-23 09:54:13 +0000

    Fix base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace YOUR_REPO_NAME with your actual GitHub repository name, e.g. "wonpon-extractor"
 export default defineConfig({
   plugins: [react()],
-  base: "/wonpon-extractor/",
+  base: "/WONPON-Extractor/",
 });
